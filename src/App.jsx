9617fca99f62f76224adeb3d29e4284ea7063a0f
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-27 17:18:34 -0400

    Add NewsFeed to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import NewsFeed from "./components/NewsFeed.jsx";
+import { Container } from "./components/styled/Container.styled.jsx";
 
 export default function App() {
+    
     return (
-        <div id="app">
-            
-        </div>
-    )
+        <Container>
+            <NewsFeed />
+        </Container>
+    );
 }
